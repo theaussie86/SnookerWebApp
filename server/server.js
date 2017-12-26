f6099a5ef312880433eea3597947b9a7bd5bd589
theaussie86 <chmurat86@gmail.com>
2017-12-26 19:31:50 +0100

    debugged

--- server/server.js
+++ server/server.js
@@ -119,7 +119,7 @@ hbs.registerHelper('formatCurrency',(num)=>{
 });
 
 hbs.registerHelper('ifCond', function(v1, v2, options) {
-    if(v1 === v2) {
+    if(v1 && v2) {
       return options.fn(this);
     }
     return options.inverse(this);
